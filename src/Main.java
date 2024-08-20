42ea2313dc1df02fe0c2836958de320d9ac86edd
Odair <odair.sp@hotmail.com>
2024-08-19 22:19:38 -0300

    Atualizado as classes conteudo, curso, mentoria

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
 import br.com.dio.desafio.dominio.Mentoria;
 
@@ -11,15 +12,25 @@ public class Main {
         Curso cursoVerilog = new Curso("Verilog", "Verilog essentials", 25);
 
         Mentoria php = new Mentoria("Introdução php", "Códigos básicos de php", LocalDate.now().minusDays(5));
-        Mentoria cplusplus = new Mentoria("Introdução C++", "Códigos básicos de C++", LocalDate.now().plusDays(5));
+        Mentoria phpPoo = new Mentoria("Poo php", "Aplicando Poo em php", LocalDate.now().minusDays(10));
+        Mentoria python = new Mentoria("Introdução Python", "Códigos básicos de Python", LocalDate.now().plusDays(5));
         Mentoria verilog = new Mentoria("Introdução a Verilog", "Códigos básicos Verilog", LocalDate.now().plusWeeks(1));
+        Mentoria verilogAplicado = new Mentoria("Aplicando Verilog", "Aplicando Verilog - Mão na Massa", LocalDate.now().plusWeeks(1));
+
+        Bootcamp pythonBootcamp = new Bootcamp();
+        pythonBootcamp.getConteudos().add(cursoPython);
+        pythonBootcamp.getConteudos().add(python);
+
+        Bootcamp phpBootcamp = new Bootcamp();
+        phpBootcamp.getConteudos().add(cursoPhp);
+        phpBootcamp.getConteudos().add(php);
+        phpBootcamp.getConteudos().add(phpPoo);
+        Bootcamp verilogBootcamp = new Bootcamp();
+        verilogBootcamp.getConteudos().add(cursoVerilog);
+        verilogBootcamp.getConteudos().add(verilog);
+        verilogBootcamp.getConteudos().add(verilogAplicado);
 
 
-        System.out.println(cursoPhp.calcularXp());
-        System.out.println(cursoVerilog);
-        System.out.println(cursoPython);
-        System.out.println(php);
-        System.out.println(cplusplus);
-        System.out.println(verilog);
+
     }
 }
